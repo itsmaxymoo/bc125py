f0fe2c2212ffe0d3aea6ca5a69bcd1b79ba6a251
itsmaxymoo <max.loiacono@protonmail.com>
2022-04-14 20:27:16 -0400

    argparse setup

--- bc125py/app.py
+++ bc125py/app.py
@@ -1,4 +1,22 @@
+import argparse
+import bc125py
+
+
+_VERBOSE = False
+
+
 def main():
-	print("Hello, World!")
+	# Create main cli parser
+	cli_parser = argparse.ArgumentParser(prog=bc125py.MODULE_NAME, description=bc125py.MODULE_DESCRIPTION)
+
+	# Add universal cli arguments
+	cli_parser.add_argument("-v", "--verbose", action="store_true")
+	cli_parser.add_argument("--version", action="version", version=bc125py.MODULE_VERSION)
+
+	# Parse arguments
+	cli_args = cli_parser.parse_args()
+	
+	# Set verbosity level
+	_VERBOSE = cli_args.verbose
 
 	return 0
