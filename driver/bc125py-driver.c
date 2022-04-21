eccb844905b65eb3bff841867e1d3d04a3187844
itsmaxymoo <max.loiacono@protonmail.com>
2022-04-21 19:03:01 -0400

    reformat

--- driver/bc125py-driver.c
+++ driver/bc125py-driver.c
@@ -24,6 +24,8 @@
 * OTHER DEALINGS IN THE SOFTWARE.
 */
 
+/* Lines 51-54 Copywright (c) 2022 Max Loiacono, MIT License */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -31,81 +33,82 @@
 #include <unistd.h>
 
 #define LINEBUF_MAX 4096
-#define ID_MAX      16
+#define ID_MAX 16
 
 void extract_value(char *, char *, char *);
 int setup_device(char *, char *);
 
 int main (int argc, char *argv[]){
-    FILE *usbdev;
-    char linebuf[LINEBUF_MAX];
-    char vendor[ID_MAX], product[ID_MAX];
-    char found_device;
+	FILE *usbdev;
+	char linebuf[LINEBUF_MAX];
+	char vendor[ID_MAX], product[ID_MAX];
+	char found_device;
 
-    vendor[0]    = 0;
-    product[0]   = 0;
-    found_device = 0;
+	vendor[0]	= 0;
+	product[0]   = 0;
+	found_device = 0;
 
-    if( (usbdev = fopen("/proc/bus/usb/devices", "r")) ){}
-    else if ( (usbdev = fopen("/sys/kernel/debug/usb/devices", "r")) ) {}
-    else { return 1; }
+	/* Find the correct usb list file */
+	if( (usbdev = fopen("/proc/bus/usb/devices", "r")) ){}
+	else if ( (usbdev = fopen("/sys/kernel/debug/usb/devices", "r")) ) {}
+	else { return 1; }
 
 
-    while( fgets(linebuf, LINEBUF_MAX, usbdev) ){
-        if(strstr(linebuf, "BC125AT")){
-            found_device   = 1;
-        }
-        else if(!linebuf[0] || !(linebuf[0] >= 'A' && linebuf[0] <= 'Z')){
-            vendor[0]  = 0;
-            product[0] = 0;
-        }
+	while( fgets(linebuf, LINEBUF_MAX, usbdev) ){
+		if(strstr(linebuf, "BC125AT")){
+			found_device   = 1;
+		}
+		else if(!linebuf[0] || !(linebuf[0] >= 'A' && linebuf[0] <= 'Z')){
+			vendor[0]  = 0;
+			product[0] = 0;
+		}
 
-        if(found_device && vendor[0] && product[0]) break;
+		if(found_device && vendor[0] && product[0]) break;
 
-        extract_value( linebuf, "Vendor=", vendor  );
-        extract_value( linebuf, "ProdID=", product );
-    }
-    
-    if (!found_device){
-        printf("found_device=%d\n", found_device);
-        return 1;
-    }
+		extract_value( linebuf, "Vendor=", vendor  );
+		extract_value( linebuf, "ProdID=", product );
+	}
+	
+	if (!found_device){
+		printf("found_device=%d\n", found_device);
+		return 1;
+	}
 
-    printf("found_device=%d; vendor=%s; product=%s\n", found_device, vendor, product);
-    
-    return setup_device(vendor, product);
+	printf("found_device=%d; vendor=%s; product=%s\n", found_device, vendor, product);
+	
+	return setup_device(vendor, product);
 }
 
 void extract_value(char *linebuf, char *match, char *result){
-    char *where;
-    short int pos;
-    where = strstr(linebuf, match);
-    if (where){
-        where += strlen(match);
-        for (pos = 0; pos < ID_MAX && *(where + pos) != 0 && *(where + pos) != ' '; pos++){
-            *(result + pos) = *(where + pos);
-        }
-        *(result + pos) = 0;
-    }
+	char *where;
+	short int pos;
+	where = strstr(linebuf, match);
+	if (where){
+		where += strlen(match);
+		for (pos = 0; pos < ID_MAX && *(where + pos) != 0 && *(where + pos) != ' '; pos++){
+			*(result + pos) = *(where + pos);
+		}
+		*(result + pos) = 0;
+	}
 }
 
 int setup_device(char *vendor, char *product){
-    uid_t orig_uid, orig_gid;
-    
-    orig_uid = getuid();
-    orig_gid = getgid();
+	uid_t orig_uid, orig_gid;
+	
+	orig_uid = getuid();
+	orig_gid = getgid();
 
-    setuid(0);
+	setuid(0);
 
-    if (getuid() != 0){
-        printf("Couldn't setuid to 0\n");
-        return 1;
-    }
-    else {
-        system("echo 1965 0017 2 076d 0006 > /sys/bus/usb/drivers/cdc_acm/new_id");
-        usleep(250 * 1000); /* sleep 250 ms before chown or, oddly, it may not take effect */
-        if (chown("/dev/ttyACM0", orig_uid, orig_gid) == -1)
-            perror("chown failed");
-    }
-    return 0;
+	if (getuid() != 0){
+		printf("Couldn't setuid to 0\n");
+		return 1;
+	}
+	else {
+		system("echo 1965 0017 2 076d 0006 > /sys/bus/usb/drivers/cdc_acm/new_id");
+		usleep(250 * 1000); /* sleep 250 ms before chown or, oddly, it may not take effect */
+		if (chown("/dev/ttyACM0", orig_uid, orig_gid) == -1)
+			perror("chown failed");
+	}
+	return 0;
 }
