b01bd1c5efe6b2d42925dc02c100550d9ce811c2
itsmaxymoo <max.loiacono@protonmail.com>
2022-04-22 16:53:36 -0400

    split_print, doc log

--- bc125py/app/log.py
+++ bc125py/app/log.py
@@ -5,14 +5,25 @@ _DEBUG: bool = False
 _FILE = sys.stdout
 
 
+def __split_print(*args) -> None:
+	"""Print message to stdout and logfile, if applicable
+	"""
+
+	print(*args)
+	if _FILE != sys.stdout:
+		print(*args, file=_FILE)
+
+
 def debug(*args) -> None:
+	"""Log debug message
+	"""
+
 	if _DEBUG:
-		print("[DEBUG]", *args)
-		if _FILE != sys.stdout:
-			print("[DEBUG]", *args, file=_FILE)
+		__split_print("[DEBUG]", *args)
 
 
 def warn(*args) -> None:
-	print("[WARN]", *args)
-	if _FILE != sys.stdout:
-		print("[WARN]", *args, file=_FILE)
+	"""Log warning
+	"""
+
+	__split_print("[WARN]", *args)
