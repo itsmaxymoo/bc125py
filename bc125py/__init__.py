3d500f93a50b8f1c0d85264122720c0d3f820371
itsmaxymoo <max.loiacono@protonmail.com>
2022-04-29 22:49:37 -0400

    shippable shell

--- bc125py/__init__.py
+++ bc125py/__init__.py
@@ -4,7 +4,7 @@ from bc125py._scanner import Scanner
 
 MODULE_NAME = "bc125py"
 MODULE_NAME_FULL = "BC125Py"
-MODULE_VERSION = "0.0.0"
+MODULE_VERSION = "0.1.0"
 MODULE_AUTHOR = "Max Loiacono"
 MODULE_AUTHOR_EMAIL = "max.loiacono@protonmail.com"
 MODULE_DESCRIPTION = "A Python-based interface for the Uniden BC125AT"
--- bc125py/app/cli.py
+++ bc125py/app/cli.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import argparse
 import datetime
@@ -119,22 +120,68 @@ def shell() -> int:
 		con = core.get_scanner_connection()
 
 		# Print header
-		print(bc125py.MODULE_NAME, "scanner shell")
-		print("use command \"exit\" to exit\n")
+		print(bc125py.MODULE_NAME, bc125py.MODULE_VERSION, "scanner shell")
+		print("try commands \"help\" or \"exit\"", os.linesep)
 
-		# Input loop
+		# User controllable variables
+		shell_echo = True
+		shell_allow_error = True
+
+		# Function for processing input/commands
+		def process_input(input: str) -> None:
+
+			nonlocal shell_echo
+			nonlocal shell_allow_error
+
+			# case: help
+			if input == "help":
+				print("help           show this text")
+				print("exit           exit the shell")
+				print("echo on        show command name in response (default)")
+				print("echo off       do not include command name in response")
+				print("error on       crash program on command error")
+				print("error off      do not crash on command error (default)")
+				print("print <text>   print all text after \"print\"")
+				print("# <text>       mark line as comment. line will be ignored")
+			
+			# case: exit, blank line, or comment
+			elif not input or input == "exit" or input.startswith("#"):
+				pass
+
+			# case: print <text>
+			elif input.startswith("print ") or input == "print":
+				print(input[6:])
+			
+			# case: echo on
+			elif input == "echo on":
+				shell_echo = True
+			
+			# case: echo off
+			elif input == "echo off":
+				shell_echo = False
+			
+			# case: error on
+			elif input == "error on":
+				shell_allow_error = False
+			
+			# case: error off
+			elif input == "error off":
+				shell_allow_error = True
+
+			# case: Input is not a special command; send input to scanner
+			else:
+				# Execute command, print result
+				print(con.exec(input, echo=shell_echo, return_tuple=False, allow_error=shell_allow_error))
+
+		# Input loop, stop if last command was "exit"
 		in_line: str = ""
 		while in_line != "exit":
 			# Get user input
 			in_line = input("> ").lstrip().rstrip()
-
-			# Skip blank lines
-			if in_line in ("", "exit"):
-				continue
 			
-			# Execute command, print result
-			print(con.exec(in_line, echo=True, return_tuple=False, allow_error=True))
-		
+			# Process input
+			process_input(in_line)
+			
 		# Close scanner connection
 		con.close()
 
