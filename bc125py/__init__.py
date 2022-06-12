4686b875f7f88afe4f7fd896d9e5a53bc5744800
itsmaxymoo <max.loiacono@protonmail.com>
2022-06-11 23:01:49 -0400

    Version update

--- bc125py/__init__.py
+++ bc125py/__init__.py
@@ -3,7 +3,7 @@ from bc125py._connection import CommandError, ScannerConnection, SimulatedScanne
 
 MODULE_NAME = "bc125py"
 MODULE_NAME_FULL = MODULE_NAME
-MODULE_VERSION = "0.4.0_indev"
+MODULE_VERSION = "0.5.0_indev"
 MODULE_AUTHOR = "Max Loiacono"
 MODULE_AUTHOR_EMAIL = "max.loiacono@protonmail.com"
 MODULE_DESCRIPTION = "A Python-based interface for the Uniden BC125AT"
