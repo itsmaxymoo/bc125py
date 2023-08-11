117fe22be846de28fd596b08efe9f719882ca19b
itsmaxymoo <max.loiacono@protonmail.com>
2023-08-11 12:27:34 -0400

    clean up mappings

--- bc125py/mappings.py
+++ bc125py/mappings.py
@@ -1,187 +1,183 @@
 """Lookup values for CTCSS and DCS codes."""
 
-# Documented string representations of special values
-NONE: str = "NONE/All"
-SEARCH: str = "SEARCH"
-NO_TONE: str = "NO_TONE"
+
+import re
+
 
 # A mapping of internal special values to human-friendly values
 SPECIAL_VALUES: dict[int, str] = {
-	0: NONE,
-	127: SEARCH,
-	240: NO_TONE,
+	0: "none",
+	127: "search",
+	240: "no_tone",
 }
 
 # A mapping of internal CTCSS values to human-friendly values
 CTCSS: dict[int, str] = {
-	64: "67.0",
-	65: "69.3",
-	66: "71.9",
-	67: "74.4",
-	68: "77.0",
-	69: "79.7",
-	70: "82.5",
-	71: "85.4",
-	72: "88.5",
-	73: "91.5",
-	74: "94.8",
-	75: "97.4",
-	76: "100.0",
-	77: "103.5",
-	78: "107.2",
-	79: "110.9",
-	80: "114.8",
-	81: "118.8",
-	82: "123.0",
-	83: "127.3",
-	84: "131.8",
-	85: "136.5",
-	86: "141.3",
-	87: "146.2",
-	88: "151.4",
-	89: "156.7",
-	90: "159.8",
-	91: "162.2",
-	92: "165.5",
-	93: "167.9",
-	94: "171.3",
-	95: "173.8",
-	96: "177.3",
-	97: "179.9",
-	98: "183.5",
-	99: "186.2",
-	100: "189.9",
-	101: "192.8",
-	102: "196.6",
-	103: "199.5",
-	104: "203.5",
-	105: "206.5",
-	106: "210.7",
-	107: "218.1",
-	108: "225.7",
-	109: "229.1",
-	110: "233.6",
-	111: "241.8",
-	112: "250.3",
-	113: "254.1"
+	64: "ctcss_67.0",
+	65: "ctcss_69.3",
+	66: "ctcss_71.9",
+	67: "ctcss_74.4",
+	68: "ctcss_77.0",
+	69: "ctcss_79.7",
+	70: "ctcss_82.5",
+	71: "ctcss_85.4",
+	72: "ctcss_88.5",
+	73: "ctcss_91.5",
+	74: "ctcss_94.8",
+	75: "ctcss_97.4",
+	76: "ctcss_100.0",
+	77: "ctcss_103.5",
+	78: "ctcss_107.2",
+	79: "ctcss_110.9",
+	80: "ctcss_114.8",
+	81: "ctcss_118.8",
+	82: "ctcss_123.0",
+	83: "ctcss_127.3",
+	84: "ctcss_131.8",
+	85: "ctcss_136.5",
+	86: "ctcss_141.3",
+	87: "ctcss_146.2",
+	88: "ctcss_151.4",
+	89: "ctcss_156.7",
+	90: "ctcss_159.8",
+	91: "ctcss_162.2",
+	92: "ctcss_165.5",
+	93: "ctcss_167.9",
+	94: "ctcss_171.3",
+	95: "ctcss_173.8",
+	96: "ctcss_177.3",
+	97: "ctcss_179.9",
+	98: "ctcss_183.5",
+	99: "ctcss_186.2",
+	100: "ctcss_189.9",
+	101: "ctcss_192.8",
+	102: "ctcss_196.6",
+	103: "ctcss_199.5",
+	104: "ctcss_203.5",
+	105: "ctcss_206.5",
+	106: "ctcss_210.7",
+	107: "ctcss_218.1",
+	108: "ctcss_225.7",
+	109: "ctcss_229.1",
+	110: "ctcss_233.6",
+	111: "ctcss_241.8",
+	112: "ctcss_250.3",
+	113: "ctcss_254.1"
 }
 
 # A mapping of internal DCS values to human-friendly values
 DCS: dict[int, str] = {
-	128: "23",
-	129: "25",
-	130: "26",
-	131: "31",
-	132: "32",
-	133: "36",
-	134: "43",
-	135: "47",
-	136: "51",
-	137: "53",
-	138: "54",
-	139: "65",
-	140: "71",
-	141: "72",
-	142: "73",
-	143: "74",
-	144: "114",
-	145: "115",
-	146: "116",
-	147: "122",
-	148: "125",
-	149: "131",
-	150: "132",
-	151: "134",
-	152: "143",
-	153: "145",
-	154: "152",
-	155: "155",
-	156: "156",
-	157: "162",
-	158: "165",
-	159: "172",
-	160: "174",
-	161: "205",
-	162: "212",
-	163: "223",
-	164: "225",
-	165: "226",
-	166: "243",
-	167: "244",
-	168: "245",
-	169: "246",
-	170: "251",
-	171: "252",
-	172: "255",
-	173: "261",
-	174: "263",
-	175: "265",
-	176: "266",
-	177: "271",
-	178: "274",
-	179: "306",
-	180: "311",
-	181: "315",
-	182: "325",
-	183: "331",
-	184: "332",
-	185: "343",
-	186: "346",
-	187: "351",
-	188: "356",
-	189: "364",
-	190: "365",
-	191: "371",
-	192: "411",
-	193: "412",
-	194: "413",
-	195: "423",
-	196: "431",
-	197: "432",
-	198: "445",
-	199: "446",
-	200: "452",
-	201: "454",
-	202: "455",
-	203: "462",
-	204: "464",
-	205: "465",
-	206: "466",
-	207: "503",
-	208: "506",
-	209: "516",
-	210: "523",
-	211: "526",
-	212: "532",
-	213: "546",
-	214: "565",
-	215: "606",
-	216: "612",
-	217: "624",
-	218: "627",
-	219: "631",
-	220: "632",
-	221: "654",
-	222: "662",
-	223: "664",
-	224: "703",
-	225: "712",
-	226: "723",
-	227: "731",
-	228: "732",
-	229: "734",
-	230: "743",
-	231: "754",
+	128: "dcs_23",
+	129: "dcs_25",
+	130: "dcs_26",
+	131: "dcs_31",
+	132: "dcs_32",
+	133: "dcs_36",
+	134: "dcs_43",
+	135: "dcs_47",
+	136: "dcs_51",
+	137: "dcs_53",
+	138: "dcs_54",
+	139: "dcs_65",
+	140: "dcs_71",
+	141: "dcs_72",
+	142: "dcs_73",
+	143: "dcs_74",
+	144: "dcs_114",
+	145: "dcs_115",
+	146: "dcs_116",
+	147: "dcs_122",
+	148: "dcs_125",
+	149: "dcs_131",
+	150: "dcs_132",
+	151: "dcs_134",
+	152: "dcs_143",
+	153: "dcs_145",
+	154: "dcs_152",
+	155: "dcs_155",
+	156: "dcs_156",
+	157: "dcs_162",
+	158: "dcs_165",
+	159: "dcs_172",
+	160: "dcs_174",
+	161: "dcs_205",
+	162: "dcs_212",
+	163: "dcs_223",
+	164: "dcs_225",
+	165: "dcs_226",
+	166: "dcs_243",
+	167: "dcs_244",
+	168: "dcs_245",
+	169: "dcs_246",
+	170: "dcs_251",
+	171: "dcs_252",
+	172: "dcs_255",
+	173: "dcs_261",
+	174: "dcs_263",
+	175: "dcs_265",
+	176: "dcs_266",
+	177: "dcs_271",
+	178: "dcs_274",
+	179: "dcs_306",
+	180: "dcs_311",
+	181: "dcs_315",
+	182: "dcs_325",
+	183: "dcs_331",
+	184: "dcs_332",
+	185: "dcs_343",
+	186: "dcs_346",
+	187: "dcs_351",
+	188: "dcs_356",
+	189: "dcs_364",
+	190: "dcs_365",
+	191: "dcs_371",
+	192: "dcs_411",
+	193: "dcs_412",
+	194: "dcs_413",
+	195: "dcs_423",
+	196: "dcs_431",
+	197: "dcs_432",
+	198: "dcs_445",
+	199: "dcs_446",
+	200: "dcs_452",
+	201: "dcs_454",
+	202: "dcs_455",
+	203: "dcs_462",
+	204: "dcs_464",
+	205: "dcs_465",
+	206: "dcs_466",
+	207: "dcs_503",
+	208: "dcs_506",
+	209: "dcs_516",
+	210: "dcs_523",
+	211: "dcs_526",
+	212: "dcs_532",
+	213: "dcs_546",
+	214: "dcs_565",
+	215: "dcs_606",
+	216: "dcs_612",
+	217: "dcs_624",
+	218: "dcs_627",
+	219: "dcs_631",
+	220: "dcs_632",
+	221: "dcs_654",
+	222: "dcs_662",
+	223: "dcs_664",
+	224: "dcs_703",
+	225: "dcs_712",
+	226: "dcs_723",
+	227: "dcs_731",
+	228: "dcs_732",
+	229: "dcs_734",
+	230: "dcs_743",
+	231: "dcs_754",
 }
 
 VALID_VALUES: dict[int, str] = {**SPECIAL_VALUES, **CTCSS, **DCS}
 
 
-def ctcss_dcs_i2h(code: int | str) -> int | float | str:
+def ctcss_dcs_to_human(code: int) -> str:
 	"""Lookup CTCSS/DCS code and return a human-friendly value.
-
-	Although multiple viable values exist for 0, 127, 240, use the value from the protocol
-	documentation.
 
 	Args:
 		code: The CTCSS/DCS code to lookup.
@@ -192,18 +188,17 @@ def ctcss_dcs_i2h(code: int | str) -> int | float | str:
 	Raises:
 		ValueError: If the code is not valid.
 	"""
-	code = int(code)
+
 	try:
 		return VALID_VALUES[code]
 	except KeyError as exc:
 		raise ValueError(f"invalid internal ctcss/dcs: {code}") from exc
 
 
-def ctcss_dcs_h2i(provided: str | float | int) -> int:
+def ctcss_dcs_to_internal(provided: str) -> int:
 	"""Map the user provided value to the internal code.
 
 	Allow various and case insensitve input for for 0, 127, and 240.
-	Allow for the user provided value to be an str, integer or float.
 
 	Args:
 		provided: The user provided value.
@@ -214,21 +209,15 @@ def ctcss_dcs_h2i(provided: str | float | int) -> int:
 	Raises:
 		ValueError: If the provided value is not valid.
 	"""
-	_provided = str(provided)
 
-	# Some convenience variations
-	if _provided.lower() in ("none", "all", "noneall"):
-		_provided = NONE
-	if _provided.lower() in ("no tone", "notone"):
-		_provided = NO_TONE
+	minimize_regex = r"[^0-9a-z]"
+
+	provided = str(provided).lower()
+	provided = re.sub(minimize_regex, "", provided)
 
 	for key, value in VALID_VALUES.items():
-		if value.lower() == _provided.lower():
+		value = re.sub(minimize_regex, "", provided)
+		if value == provided:
 			return key
-	_provided = _provided.lower()
 
-	valid_values = f"Special: [{', '.join(SPECIAL_VALUES.values())}]"
-	valid_values += f" CTCSS: [{', '.join(CTCSS.values())}]"
-	valid_values += f" DCS: [{', '.join(DCS.values())}]"
-	raise ValueError(
-		f"invalid provided ctcss/dcs: {provided}, valid values: {valid_values}")
+	raise ValueError("Invalid input value: " + str(provided))
