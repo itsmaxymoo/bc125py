15e78f809353048a625e02c5a6a4635f565bd6b2
itsmaxymoo <max.loiacono@protonmail.com>
2023-08-11 16:53:42 -0400

    up version

--- bc125py/__init__.py
+++ bc125py/__init__.py
@@ -1,6 +1,6 @@
 PACKAGE_NAME = "bc125py"
 PACKAGE_NAME_FULL = PACKAGE_NAME
-PACKAGE_VERSION = "0.9.10"
+PACKAGE_VERSION = "0.9.11"
 PACKAGE_AUTHOR = "Max Loiacono"
 PACKAGE_AUTHOR_EMAIL = "max.loiacono@protonmail.com"
 PACKAGE_DESCRIPTION = "A Python-based interface for the Uniden BC125AT"
